5850a1b00c777d4733f831c31b81ca0b648665fe
mhulic <michal.hulic2@gmail.com>
2024-04-24 16:50:31 +0200

    docker iomage scan was added

--- vars/dockerImageScan.groovy
+++ vars/dockerImageScan.groovy
@@ -1,5 +1,4 @@
-// def call(String project, String ImageTag, String hubUser){
-    
+def call(String project, String ImageTag, String hubUser){
     sh """
      trivy image ${hubUser}/${project}:latest > scan.txt
      cat scan.txt
